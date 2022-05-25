61d7782540eb358faf23f82b21d1b5b564a74a99
CtkGHoSt <39295159+CtkGHoSt@users.noreply.github.com>
2022-05-25 21:41:13 +0800

    增加列表筛选

--- pjj.py
+++ pjj.py
@@ -5,6 +5,8 @@ import json
 class Pjj:
     @staticmethod
     def _split(split_string):
+        if len(split_string) == 0:
+            return '', None
         split_list = split_string.split('.')
         res = ''
         for item in split_list:
@@ -23,22 +25,75 @@ class Pjj:
                 key = key[:rk_index] + r'\S' + key[rk_index:]
         _ = list(filter(lambda x: re.fullmatch(key, x) is not None,tmp_obj.keys()))
         if len(_) != 1:
-            raise IndexError('key not found：{}/{}'.format(key, tmp_obj.keys()))
+            # raise IndexError('key not found：{}/{}'.format(key, tmp_obj.keys()))
+            return None
         return _[0]
+    
+    @staticmethod
+    def _judge_eval_params(params:str, tmp_obj):
+        '''
+        只能使用比较方法，且只能用tmp_obj的key作比较
+        #(count>=5 && name=="huian")
+        #(tail>15&&tail!=22||tail==11)
+
+        eval(params)不够安全
+        '''
+        logical = [r'[&&]+', r'[||]+']
+        comparison = [r'>[=]?', r'<[=]?', r'[!,=]?=']
+        pp = [params,]
+        cache = list()
+        new_pp = list()
+        for operator in logical + comparison: # 先处理逻辑运算符，再处理比较运算符
+            new_pp = list()
+            for i in pp:
+                re_list = re.findall(operator, i)
+                if len(re_list) == 0:
+                    new_pp.append(i)
+                    continue
+                for j in re_list:
+                    cache = i.split(j)
+                new_pp += cache
+            pp = new_pp
+            cache = list()
+        f_string = list(filter(lambda x:x not in list(tmp_obj.keys()),pp))
+        for p in f_string:
+            try:
+                float(p)
+            except ValueError:
+                if not (p[0] == '"' and p[-1] == '"' or p[0] == "'" and p[-1] == "'"):
+                    raise ValueError(p)
+
+        for key in tmp_obj.keys():
+            params = params.replace(key, 'tmp_obj["{}"]'.format(key))
+        params = params.replace('&&', ' and ').replace('||', ' or ')
+        return eval(params)
 
     def _get_value(self, key, tail, tmp_obj):
         if isinstance(tmp_obj, list):
+            
             if key == '#':
                 if tail is None:
                     return len(tmp_obj)
                 return tmp_obj
             try:
                 return tmp_obj[int(key)]
-            except ValueError:
-                return [ i[self._get_re_key(key, i)] for i in tmp_obj]
+            except IndexError:
+                return None
+            except ValueError: # key 不为整型
+                try:
+                    if re.fullmatch(r'#(\S+)', key):
+                        exec_string = re.fullmatch(r'#(\S*)', key).group()[2:-1]
+                        tmp_obj = [i for i in tmp_obj if self._judge_eval_params(exec_string, i)]
+                        return tmp_obj
+                    return [ i[self._get_re_key(key, i)] for i in tmp_obj]
+                except KeyError:
+                    return None
         
         re_key = self._get_re_key(key, tmp_obj)
-        return tmp_obj[re_key]
+        try:
+            return tmp_obj[re_key]
+        except KeyError:
+            return None
 
     def make_res(self):
         head, tail = self._split(self.base_string)
